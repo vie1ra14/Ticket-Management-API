eda7145d4a7488cb7813c42c17bf38054507930e
Valdemar Nanon <hunternanon@gmail.com>
2026-09-24 16:24:03 -0300

    feat: remove components criado a mão para substituição de styled-components e components antd

--- frontend/src/components/Input/Input.tsx
+++
@@ -1,8 +0,0 @@
-import type { InputHTMLAttributes } from "react"
-import styles from "./Input.module.css"
-
-type InputProps = InputHTMLAttributes<HTMLInputElement>
-
-export function Input({ className = "", ...props }: InputProps) {
-    return <input className={`${styles.input} ${className}`.trim()} {...props} />
-}
